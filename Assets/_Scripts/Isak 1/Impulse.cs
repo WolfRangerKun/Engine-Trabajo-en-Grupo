9959505d6c2c4a2c24e464028591cbbaddb92b5a
WolfRangerKun <80328108+WolfRangerKun@users.noreply.github.com>
2021-11-11 13:49:34 -0300

    fgfdfgd

--- Assets/_Scripts/Isak 1/Impulse.cs
+++ Assets/_Scripts/Isak 1/Impulse.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Impulse : MonoBehaviour
 {
     public float impulse;
+    bool canJump = true;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && canJump)
         {
 
             other.GetComponent<Rigidbody>().AddForce(transform.forward * impulse /3*2 * Time.deltaTime, ForceMode.Impulse);
             other.GetComponent<Rigidbody>().AddForce(Vector3.up * impulse * 5f * Time.deltaTime, ForceMode.Impulse);
+            canJump = false;
+            StartCoroutine("active");
         }
+    }
+
+
+
+    IEnumerator active()
+    {
+        yield return new WaitForSeconds(2);
+        canJump = false;
+        yield break;
     }
 }
--- Assets/_Scripts/Isak 1/RotatePlataform.cs
+++ Assets/_Scripts/Isak 1/RotatePlataform.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class RotatePlataform : MonoBehaviour
 {
     public bool canMove;
-    private void FixedUpdate()
+    public Transform toRotate;
+    Quaternion saveRotation;
+   
+
+    private void OnMouseDrag()
     {
-        if (canMove && Input.GetMouseButton(0))
+        if (canMove )
         {
-            Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
-            Vector3 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-            Vector3 direction = mouseOnScreen - positionOnScreen;
-
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg /*- 90f*/;
-            transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
-
+            Rotation();
         }
-
     }
-
     public void ChangeStatus()
     {
         canMove = !canMove;
+    }
+
+    void Rotation()
+    {
+        Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(toRotate.position);
+        Vector3 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        Vector3 direction = mouseOnScreen - positionOnScreen;
+
+        float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f) + saveRotation.y;
+
+
+        toRotate.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+
     }
 }

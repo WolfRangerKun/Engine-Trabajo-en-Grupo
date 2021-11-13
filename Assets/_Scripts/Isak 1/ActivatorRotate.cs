5ce6ed1f9424670ac4317e123e325d00ff300e1e
Avocad0NT <80328273+Avocad0NT@users.noreply.github.com>
2021-11-13 18:55:31 -0300

    Wea

--- Assets/_Scripts/Isak 1/ActivatorRotate.cs
+++ Assets/_Scripts/Isak 1/ActivatorRotate.cs
@@ -5,13 +5,20 @@ using UnityEngine.Events;
 
 public class ActivatorRotate : MonoBehaviour
 {
+    public List<GameObject> objetosCambioMaterial;
+    public Material original;
+    public Material nuevo;
+    public GameObject flecha;
+
     public UnityEvent actionsIn,actionsOut;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            flecha.SetActive(true);
             actionsIn?.Invoke();
+            CambioMaterialGlow();
         }
     }
 
@@ -19,7 +26,24 @@ public class ActivatorRotate : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            flecha.SetActive(false);
             actionsOut?.Invoke();
+            CambioMaterialOriginal();
+        }
+    }
+
+    public void CambioMaterialGlow()
+    {
+        for (int i = 0; i < objetosCambioMaterial.Count; ++i)
+        {
+            objetosCambioMaterial[i].GetComponent<MeshRenderer>().material = nuevo;
+        }
+    }
+    public void CambioMaterialOriginal()
+    {
+        for (int i = 0; i < objetosCambioMaterial.Count; ++i)
+        {
+            objetosCambioMaterial[i].GetComponent<MeshRenderer>().material = original;
         }
     }
 }
